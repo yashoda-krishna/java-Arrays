dc285c35c9697b97f7e21688f788511c13138a9f
yashoda-krishna <148547478+yashoda-krishna@users.noreply.github.com>
2024-06-29 15:57:46 +0530

    Add files via upload

---
+++ M29.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class M29 {
+	public static void main(String[] args) {
+		int[] nums = {10,20,30,40,50,60};
+		System.out.println("Initial content:" + Arrays.toString(nums));
+		for(int i = nums.length - 1;i > 1; i--) {
+			nums[i] = nums[i - 2];
+		}
+		System.out.println("Final content:" + Arrays.toString(nums));
+	}
+}
---
+++ M30.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class M30 {
+public static void main(String[] args) {
+	int[] nums = {10,20,30,40,50,60};
+	System.out.println("Initial content:" + Arrays.toString(nums));
+	int t1 = nums[nums.length - 1];
+	int t2 = nums[nums.length - 2];
+	for(int i = nums.length - 1;i > 1; i--) {
+		nums[i] = nums[i - 2];
+	}
+	nums[1] = t1;
+	nums[2] = t2;
+	System.out.println("Final content:" + Arrays.toString(nums));
+
+}
+}
